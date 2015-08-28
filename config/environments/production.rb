2cc152f2360901980595503fd833e39356356a64
Efrat Abramovitz <efrat.abramovitz@gmail.com>
2015-08-28 17:30:41 +0300

    Implemented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    #used by heroku
+    #actual host
+  config.action_mailer.default_url_options = { :host => 'www.efratoio.com' }
 end
